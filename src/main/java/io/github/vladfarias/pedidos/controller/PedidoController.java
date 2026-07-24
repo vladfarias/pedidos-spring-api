f6d081b93e6a908801b82192f37fe299140011f0
vladfarias <vladfarias@gmail.com>
2026-07-24 20:23:10 -0300

    feat: integra controller ao service de pedidos

--- src/main/java/io/github/vladfarias/pedidos/controller/PedidoController.java
+++ src/main/java/io/github/vladfarias/pedidos/controller/PedidoController.java
@@ -1,12 +1,25 @@
 package io.github.vladfarias.pedidos.controller;
 
+import io.github.vladfarias.pedidos.dto.PedidoResponseDTO;
+import io.github.vladfarias.pedidos.service.PedidoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
-    @GetMapping("/pedidos")
-    public String listarPedidos(){
-        return "Lista de pedidos";
+
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @GetMapping
+    public List<PedidoResponseDTO> listarTodos() {
+        return pedidoService.listarTodos();
     }
 }
